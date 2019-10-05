2996d12f140f2c0d10cf2988fd938383763c2e00
Avinash Bhalavi <abhalavi@moreyeahs.in>
2019-10-05 11:42:47 +0530

    first commit sabelo website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from "./layouts/main-page/main/main.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'layout',
+    pathMatch: 'full',
+  },
+  {
+    path: 'layout',
+    component: MainComponent,
+    children: [
+      
+       { path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)}, // Home
+      // { path: 'admin', loadChildren: () => import('./A_SuperAdmin/admin.module').then(m => m.AdminModule) },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainComponent } from './layouts/main-page/main/main.component';
+
+//Font awesome
+//import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  //  AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
